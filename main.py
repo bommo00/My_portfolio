866998aa7aa985ea84a6041fbb3b117457bf9946
MOBOLLNE <1525996738@qq.com>
2023-11-07 19:46:06 +0900

    Basically finish!

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
+import io,sys
+import os
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 app = Flask(__name__)
+
+
+#for message
+MY_EMAIL = os.environ.get('EMAIL')
+
+MY_PASSWORD = os.environ.get('KEY')
+
+OTHER_EMAIL = os.environ.get('OTHER')
+
+
+
 
 @app.route("/")
 def home():
@@ -8,11 +23,25 @@ def home():
 
 @app.route("/artwork")
 def gallery():
+
     return render_template("generic.html")
 
-@app.route("/aboutme")
+@app.route("/aboutme",methods=["GET", "POST"])
 def self_intro():
-    return render_template("self.html")
+    if request.method == 'POST':
+        data = request.form
+        with smtplib.SMTP("outlook.office365.com", port=587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            content = f"Subject:A new comment from{data['email_address']}\n\n{data['comment']}"
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=OTHER_EMAIL,
+                msg=content
+            )
+        return render_template("self.html", send=True)
+
+    return render_template("self.html", send=False)
 
 @app.route("/commission")
 def prize():
